eabeffd91046ed873df83897b6f15bdd72779554
lac641 <lawrenceleo579@gmail.com>
2025-10-26 16:33:52 +0100

    backend complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,8 @@ import imagekit from "../configs/imageKit.js";
 import User from "../models/User.js";
 import fs from "fs";
 import Connection from "../models/Connection.js";
+import Post from "../models/Post.js";
+import { inngest } from "../inngest/index.js";
 
 
 //Get user Data using userId
@@ -205,9 +207,14 @@ export const sendConnectionRequest = async (req, res) => {
     })
 
     if(!connection){
-      await connection.create({
+      const newConnection = await Connection.create({
         from_user_id: userId,
         to_user_id: id
+      })
+
+      await inngest.send({
+        name:'app/connection-request',
+        data:{connectionId: newConnection._id}
       })
 
       return res.json({
@@ -292,4 +299,22 @@ export const acceptConnectionRequest = async (req, res) => {
   }
 };
 
+//Get user profiles
+export const getUserProfiles = async (req, res) => {
+  try {
+    const {profileId} = req.body;
+    const profile = await User.findById(profileId)
+    if(!profile){
+      return res.json({success: false, message:'Profile not found'});
+    }
+    const posts = await Post.find({user: profileId}).populate('user')
+    res.json({success:true, profile, posts})
+
+
+    
+  } catch (error) {
+         console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
+}
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updateUserData } from '../controllers/userController.js'
+import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, getUserProfiles, sendConnectionRequest, unfollowUser, updateUserData } from '../controllers/userController.js'
 import { protect } from '../middlewares/auth.js'
 import { upload } from '../configs/multer.js'
+import { getUserRecentMessages } from '../controllers/messageController.js'
 
 const userRouter = express.Router()
+
 
 userRouter.get('/data',protect, getUserData)
 userRouter.post('/update',upload.fields([{name: 'profile', maxCount:1}, {name: 'cover', maxCount:1}]),protect, updateUserData)
@@ -13,6 +15,9 @@ userRouter.post('/unfollow',protect, unfollowUser)
 userRouter.post('/connect', protect, sendConnectionRequest)
 userRouter.post('/accept', protect, acceptConnectionRequest)
 userRouter.get('/connections', protect, getUserConnections)
+
+userRouter.post('/profiles', getUserProfiles)
+userRouter.get('/recent-messages', protect, getUserRecentMessages)
 
 
 export default userRouter
